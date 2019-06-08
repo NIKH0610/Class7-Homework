553c4f12ebffcf649b7e3ac1e2543fe46e1fbe7b
Nikhil <nikhil.rajendrakumar@gmail.com>
2019-06-07 22:46:36 -0400

    Updated on 07 June

---
+++ Advanced_Project_Trial.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import sklearn
+from sklearn.cluster import KMeans
+
+from sklearn import datasets
+boston = datasets.load_boston()
+bs = pd.DataFrame(boston.data)
+bs.columns = boston.feature_names
+bs['Target'] = pd.Series(boston.target,index = bs.index)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(bs[['AGE', 'DIS']], bs['Target'], test_size = 0.35)
+kme = KMeans(n_clusters=3, random_state=0).fit(x_train)
+kme.predict(x_test)
+s = kme.score(x_test)
+
+print(s)
+
+
+
