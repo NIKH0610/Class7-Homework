eb65b41481e1e6276028eccda40581dcad561a10
Nikhil <nikhil.rajendrakumar@gmail.com>
2019-06-07 20:36:19 -0400

    Full Basic Question

--- Project_Trial.py
+++ Project_Trial.py
@@ -6,12 +6,15 @@ import seaborn as sns
 
 from sklearn import datasets
 boston = datasets.load_boston()
-bs = pd.DataFrame(data=mp.c_[boston.data, boston.target], columns = boston.feature_names + ['target'])
-mean_CRIM = bs["CRIM"].mean()
+bs = pd.DataFrame(boston.data)
+bs.columns = boston.feature_names
+bs['Target'] = pd.Series(boston.target,index = bs.index)
+
+mean_CRIM = bs['CRIM'].mean()
 
 #Anything more than Mean in CRIM is considered to be high CRIME rate
 
-print(mean_CRIM)
+#print(mean_CRIM)
 
 #Removing Rows which is less than mean CRIM index
 
@@ -20,10 +23,13 @@ CRIM_high = bs.drop(bs[bs.CRIM<mean_CRIM].index)
 
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(CRIM_high, CRIM_high["MEDV"], size = 0.20)
+x_train, x_test, y_train, y_test = train_test_split(CRIM_high, CRIM_high["Target"], test_size = 0.35)
 
 from sklearn.linear_model import LinearRegression
 linear = LinearRegression()
 linear.fit(x_train, y_train)
-linear.predict(x_test)
+prediction = linear.predict(x_test)
+
+score = linear.score(x_test, y_test)
+print(score)
 
