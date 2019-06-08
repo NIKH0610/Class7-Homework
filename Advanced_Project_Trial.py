10b5eb376c2a8e648fa25288b01d36108c012f92
Nikhil <nikhil.rajendrakumar@gmail.com>
2019-06-07 22:53:08 -0400

    Final Update Advanced

--- Advanced_Project_Trial.py
+++ Advanced_Project_Trial.py
@@ -10,11 +10,10 @@ bs['Target'] = pd.Series(boston.target,index = bs.index)
 
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(bs[['AGE', 'DIS']], bs['Target'], test_size = 0.35)
-kme = KMeans(n_clusters=3, random_state=0).fit(x_train)
-kme.predict(x_test)
-s = kme.score(x_test)
-
-print(s)
-
+for i in range(0, 4):
+    kme = KMeans(n_clusters=(3+i*2), random_state=0).fit(x_train)
+    kme.predict(x_test)
+    s = kme.score(x_test)
+    print(s)
 
 
